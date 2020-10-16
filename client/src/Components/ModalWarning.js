37527a9b42a8313fd617046df31817740d576b32
AnnaCK <anna.czekajlo.kozlowska@gmail.com>
2020-10-16 19:05:21 +0200

    handle modal warning style and logic

--- client/src/Components/ModalWarning.js
+++ client/src/Components/ModalWarning.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
+import { ModalBlue, ModalBlueLight } from '../theme/Colors';
 
 const ModalBackground= styled.div`
     width: 100vw;
@@ -16,8 +17,72 @@ const ModalBackground= styled.div`
 `;
 
 const ModalContent= styled.div`
-    background-color: red;
-    padding: 5rem
+    background-color: white;
+    border: 6px solid ${ModalBlue};
+    border-radius: 7px;
+    padding: 1rem;
+    width: 50%;
+    @media (max-width: 1000px) {
+      width: 70%;
+    }
+`;
+
+const ButtonX = styled.button`
+    background-color: inherit;
+    font-size: 1.5rem;
+    border: none;
+    width: 3rem;
+    margin: 0 auto;
+    float: right;
+    padding: 0rem 0rem 0.5rem 0.5rem;
+    &:hover {
+      color: red;
+      font-weight: 900
+    }
+`;
+
+const Text= styled.h1`
+    text-align: center;
+    font-size: 0.9rem;
+    width: 70%;
+    margin: 0 auto;
+    padding: 2.5rem 2rem 1rem 2rem;
+    line-height: 1.2rem;
+    @media (max-width: 1000px) {
+      font-size: 0.8rem;
+    }
+`;
+
+const Buttons= styled.div`
+    width: 70%;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    @media (max-width: 950px) {
+      width: 90%
+    }
+`;
+
+const Button = styled.button`
+    background-color: white;
+    font-size: 0.8rem;
+    border: 2px solid grey;
+    border-radius: 4px;
+    width: 10rem;
+    height: 3rem;
+    margin: 0.5rem;
+    padding: 0.5rem;
+    &:hover {
+      background-color: ${({ warning }) => (warning ? 'tomato' : 'seagreen')};
+      color: white;
+    }
+    @media (max-width: 1050px) {
+      font-size: 0.75rem;
+      width: 8rem;
+      height: 3.5rem;
+    }
 `;
 
 function Portal (props) {
@@ -35,10 +100,12 @@ function ModalWarning (props) {
       <Portal>
       <ModalBackground>
         <ModalContent>
-          <button onClick={props.onCloseModalWarning}>x</button>
-          <h1>You have unsaved changes on your packing list. Creating new list without saving changes will cause data loss</h1>
-          <button onClick={props.onConfirmWithoutSaving}>Create a new empty list without saving changes</button>
-          <button onClick={props.onSaveAndConfirm}>Save current list before creating a new one</button>
+          <ButtonX onClick={props.onCloseModalWarning}>x</ButtonX>
+          <Text>You have unsaved changes on your packing list. <br/>{props.textMain} without saving changes will cause data loss.</Text>
+          <Buttons>
+            <Button onClick={props.onSaveAndConfirm} warning={false}>{props.textSave}</Button>
+            <Button onClick={props.onConfirmWithoutSaving} warning={true}>{props.textDoNotSave}</Button>
+          </Buttons>
         </ModalContent>
       </ModalBackground>
       </Portal>
