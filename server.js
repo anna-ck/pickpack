8b162784fa057ca5b3c50727b26c721711638804
AnnaCK <anna.czekajlo.kozlowska@gmail.com>
2020-11-02 12:16:32 +0100

    add path to server.js

--- server.js
+++ server.js
@@ -37,6 +37,14 @@ db.mongoose
 const User = db.user
 const Items = db.items
 
+const path = require('path')
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 app.use(function(req, res, next) {
   res.header(
     "Access-Control-Allow-Headers",
